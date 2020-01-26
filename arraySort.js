dc7c1fd082d3a5ba3b34216541681bf2d3c5348e
zikzager <60246677+zikzager@users.noreply.github.com>
2020-01-26 21:18:49 +0700

    Update arraySort.js

--- arraySort.js
+++ arraySort.js
@@ -7,7 +7,7 @@ console.log(arr);//генерация массива
 
 function noobSort(arrToSort){
     let arrSorted = [];
-        for (let i = arrToSort.length; i > 0; i -= 1){
+    for (let i = arrToSort.length; i > 0; i -= 1){
         let x=arrToSort.pop();
         for (let j = 0; j < arrToSort.length; j+= 1) {
             if (x > arrToSort[j]) {
