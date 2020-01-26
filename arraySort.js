8cb9512ebf17d9fddd150a0fa997ff6559ea8361
zikzager <60246677+zikzager@users.noreply.github.com>
2020-01-26 21:16:55 +0700

    Update arraySort.js

--- arraySort.js
+++ arraySort.js
@@ -1,24 +1,23 @@
 'use strict';
 let arr = [];
-
-for (let i=20;i>0;--i){
+for (let i = 20; i > 0; i -= 1) {
     arr.push(Math.floor(Math.random()*100));
-}  console.log(arr);//генерация массива
+}
+console.log(arr);//генерация массива
 
 function noobSort(arrToSort){
-let arrSorted = [];
-let x;
-let a;
-for (let i=arrToSort.length; i>0; --i){
-    x=arrToSort.pop();
-    for (let j=0; j<20;++j){
-        if (x>arrToSort[j]){
-            let a=arrToSort[j];
-            arrToSort[j]=x;
-            x=a;
-        } }
+    let arrSorted = [];
+        for (let i = arrToSort.length; i > 0; i -= 1){
+        let x=arrToSort.pop();
+        for (let j = 0; j < arrToSort.length; j+= 1) {
+            if (x > arrToSort[j]) {
+                let a = arrToSort[j];
+                arrToSort[j] = x;
+                x = a;
+            } 
+        }
     arrSorted.push(x);
+    }
+    return arrSorted;
 }
-console.log(arrSorted);
-}
-noobSort(arr)
+console.log(noobSort(arr))
